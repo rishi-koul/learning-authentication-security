929d067f4a27eb85ddbd262b9044ac5ee3c73102
Rishi <rishiu@gmail.com>
2021-07-29 21:11:06 +0530

    Final Level: Using google for auth

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ const bodyParser = require("body-parser")
 const session = require("express-session")
 const passport = require("passport")
 const passportLocalMongoose = require("passport-local-mongoose")
-const bcrypt = require("bcrypt")
-const saltRounds = 10
+const GoogleStrategy = require("passport-google-oauth20").Strategy
+const findOrCreate = require("mongoose-findorcreate")
 
 const app = express()
 app.set('view engine', 'ejs')
@@ -29,17 +29,39 @@ mongoose.set("useCreateIndex", true)
 
 const userSchema = new mongoose.Schema({
     email: String,
-    password: String
+    password: String,
+    googleId: String
 })
 
 userSchema.plugin(passportLocalMongoose)
+userSchema.plugin(findOrCreate)
 
 const User = mongoose.model("User", userSchema)
 
 passport.use(User.createStrategy())
 
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+  });
+  
+  passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+      done(err, user);
+    });
+  });
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets",
+    userProfileURL:"https://www.googleapis.com/oauth2/v3/userinfo"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
 
 app.get("/", function(req, res){
     res.render("home")
@@ -95,6 +117,16 @@ app.get("/logout", function(req, res){
     req.logOut()
     res.redirect("/")
 })
+
+app.get('/auth/google',
+  passport.authenticate('google', { scope: ['profile'] }));
+
+app.get('/auth/google/secrets', 
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/secrets');
+  });
 
 app.listen(3000, function(){
     console.log("Server started at port 3000")
